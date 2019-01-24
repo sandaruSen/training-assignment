8aee7d6b5d069101dadf70412057e27ad527a679
sandaru <sandaru.14@cse.mrt.ac.lk>
2019-01-24 10:42:23 +0530

    test

---
+++ sun-travels-rest/src/main/java/it/codegen/assignment/service/Test.java
@@ -0,0 +1,28 @@
+package it.codegen.assignment.service;
+
+public class Test
+{
+	public int getA()
+	{
+		return a;
+	}
+
+	public void setA( int a )
+	{
+		this.a = a;
+	}
+
+	public int getC()
+	{
+		return c;
+	}
+
+	public void setC( int c )
+	{
+		this.c = c;
+	}
+
+	private int a;
+	private int c;
+
+}
